a66907abbb0c3df339ce38052fb2de8efad69b88
Akhil Kannan <143798318+Alverok@users.noreply.github.com>
2026-05-16 06:25:39 +0530

    feat(chore): https implementation done (#7)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    basicSsl(),
+  ],
+
   server: {
     port: 5173,
-    proxy: {
-      '/auth':  { target: 'http://localhost:3000', changeOrigin: true, secure: false },
-      '/submissions': { target: 'http://localhost:3000', changeOrigin: true, secure: false },
-      '/admin': { target: 'http://localhost:3000', changeOrigin: true, secure: false },
-    },
   },
 });
